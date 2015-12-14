f3c4df4f2cd3e635cc1c6a6338475b5d7a971daa
crgp01 <khristhin_r@hotmail.com>
2015-12-13 22:15:25 -0500

    Creacion de proyecto tesis
    Creacion de nuevo proyecto en sails y layouts

---
+++ TESIS/PlanoAccesible/README.md
@@ -0,0 +1,3 @@
+# PlanoAccesible
+
+a [Sails](http://sailsjs.org) application
---
+++ TESIS/PlanoAccesible/api/controllers/InformacionController.js
@@ -0,0 +1,11 @@
+/**
+ * InformacionController
+ *
+ * @description :: Server-side logic for managing Informacions
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
